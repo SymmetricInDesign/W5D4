97c91a063c1c5a8689d74bf97414a4e2892de635
wliu <wliu0709@gmail.com>
2021-05-20 14:48:08 -0400

    updated course modle

--- Associations/app/models/course.rb
+++ Associations/app/models/course.rb
@@ -9,5 +9,19 @@ class Course < ApplicationRecord
         through: :enrollments,
         source: :user
 
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
+
+    has_many :advance_courses,
+        class_name: :Course,
+        primary_key: :id,
+        foreign_key: :prereq_id
+
+    belongs_to :pre_course,
+        class_name: :Course,
+        primary_key: :id,
+        foreign_key: :prereq_id
     
 end
