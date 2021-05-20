9caa87e708efa23fc650504322e81fb509e6ff4b
wliu <wliu0709@gmail.com>
2021-05-20 16:48:37 -0400

    updated shortenedurl

---
+++ URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,19 @@
+require 'securerandom'
+class ShortenedUrl < ApplicationRecord
+    validates :user_id, presence: true, uniqueness: true
+    validates :short_url, presence: true, uniqueness: true
+    validates :long_url, presence: true
+
+    def self.random_code 
+        while true
+            code = SecureRandom.urlsafe_base64(6)
+            next if !ShortenedUrl.exists?(:short_url => code)
+            return code 
+        end   
+    end 
+
+    def self.create_url(user, long_url)
+        ShortenedUrl.create!({short_url: ShortenedUrl.random_code, long_url: long_url, user_id: user.id})
+    end 
+
+end
